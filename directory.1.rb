4a508ca241a6f621e26f020be42d4f3e3754317d
Alessa <alessa_hardwick@live.co.uk>
2020-06-02 14:29:52 +0000

    altered print method to puts students name's beginning with The

--- directory.1.rb
+++ directory.1.rb
@@ -18,8 +18,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each do |student|
+    if student[:name].to_s.start_with?("The") == true
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
