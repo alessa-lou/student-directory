52041b88c8b2188253ac55fee4697be19f9b1fdc
Alessa <alessa_hardwick@live.co.uk>
2020-06-02 14:56:25 +0000

    altered hash to include hobbies

--- directory.1.rb
+++ directory.1.rb
@@ -1,16 +1,16 @@
 #First we print the list of students
 students =  [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
+  {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "murdering people"},
+  {name: "Darth Vader", cohort: :november, hobbies: "murdering younglings"},
+  {name: "Nurse Ratched", cohort: :november, hobbies: "murdering people"},
+  {name: "Michael Corleone", cohort: :november, hobbies: "mafia stuff"},
+  {name: "Alex DeLarge", cohort: :november, hobbies: "being weird"},
+  {name: "The Wicked Witch of the West", cohort: :november, hobbies: "flying monkeys"},
+  {name: "Terminator", cohort: :november, hobbies: "robotics"},
+  {name: "Freddy Krueger", cohort: :november, hobbies: "dream creeping"},
+  {name: "The Joker", cohort: :november, hobbies: "being sad inside"},
+  {name: "Joffrey Baratheon", cohort: :november, hobbies: "torturing"},
+  {name: "Norman Bates", cohort: :november, hobbies: "poisoning"}
 ]
 def print_header
   puts "The students of Villains Academy"
@@ -21,7 +21,7 @@ def print(students)
   counter = 0
   while counter != 11 do
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]} enjoys #{student[:hobbies]}"
     counter += 1
     end
   end
