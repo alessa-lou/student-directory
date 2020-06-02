92e42f7e7d76f6b8ef7762f9e5e2b181c47772d4
Alessa <alessa_hardwick@live.co.uk>
2020-06-02 14:31:46 +0000

    altered print method to print students with names shorter than 12 characters

--- directory.1.rb
+++ directory.1.rb
@@ -19,7 +19,7 @@ end
 
 def print(students)
   students.each do |student|
-    if student[:name].to_s.start_with?("The") == true
+    if student[:name].to_s.length <= 12
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
