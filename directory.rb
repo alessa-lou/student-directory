26369a67aae471eea8b4b94d2795d39491e72e1d
Alessa <alessa_hardwick@live.co.uk>
2020-06-05 14:00:25 +0000

    14.8 reading files own source code

--- directory.rb
+++ directory.rb
@@ -121,6 +121,12 @@ end
 
 
 
-default_startup_load
-interactive_menu
+File.open(__FILE__, "r") do |file|
+   file.readlines.each do |line|
+    puts line
+  end 
+end
+
+#default_startup_load
+#interactive_menu
 
